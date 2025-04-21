96b7b0691e65add5eeac57ad63556342d6ede9dc
HoVanNhuan <honhuan2k2@gmail.com>
2025-04-21 09:23:14 +0700

    update

---
+++ src/main/java/com/hvnhuan/identity_service/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.hvnhuan.identity_service.exception;
+
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorize Exception error"),
+    INVALID_KEY(1001, "Invalid message key"),
+    USER_EXITED(1002, "User exited"),
+    USERNAME_INVALID (1003, "Username must be at least 3 character !"),
+    INVALID_PASSWORD (1004, "Password must be at least 8 character !")
+    ;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    private int code;
+    private String message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
